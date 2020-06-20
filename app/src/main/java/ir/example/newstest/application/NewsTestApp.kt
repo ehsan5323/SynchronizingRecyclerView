4478c6e1f47d8583165cc93119cd758d929e192a
Ehsan Ahmadi <ehsan5323@gmail.com>
2020-06-20 21:36:04 +0430

    DaggerAppComponent

--- app/src/main/java/ir/example/newstest/application/NewsTestApp.kt
+++ app/src/main/java/ir/example/newstest/application/NewsTestApp.kt
@@ -2,6 +2,7 @@ package ir.example.newstest.application
 
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import ir.example.newstest.di.component.DaggerAppComponent
 
 class NewsTestApp : DaggerApplication() {
     override fun onCreate() {
