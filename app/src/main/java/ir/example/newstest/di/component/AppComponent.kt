9d4fd3dfe08ad70d8da7a8cf35acc9f77d4aaad8
Ehsan Ahmadi <ehsan5323@gmail.com>
2020-06-20 19:40:08 +0430

    add restful api for call network

--- app/src/main/java/ir/example/newstest/di/component/AppComponent.kt
+++ app/src/main/java/ir/example/newstest/di/component/AppComponent.kt
@@ -8,6 +8,8 @@ import dagger.android.support.AndroidSupportInjectionModule
 import ir.example.newstest.application.NewsTestApp
 import ir.example.newstest.di.builder.ActivityBuilder
 import ir.example.newstest.di.module.AppModule
+import ir.example.newstest.di.module.DatabaseModule
+import ir.example.newstest.di.module.NetworkModule
 import javax.inject.Singleton
 
 @Singleton
@@ -15,12 +17,14 @@ import javax.inject.Singleton
     modules = [
         (AndroidSupportInjectionModule::class),
         (AppModule::class),
+        (NetworkModule::class),
+        (DatabaseModule::class),
         (ActivityBuilder::class)]
 )
 interface AppComponent : AndroidInjector<NewsTestApp> {
 
     @Component.Factory
-    abstract class Builder : AndroidInjector.Factory<NewsTestApp>{
+    abstract class Builder : AndroidInjector.Factory<NewsTestApp> {
         interface Factory {
             fun create(@BindsInstance application: Context): AppComponent
         }
