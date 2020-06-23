08667a026633b927fb99691ffdffcc735d6e3431
Ehsan Ahmadi <ehsan5323@gmail.com>
2020-06-23 19:40:06 +0430

    add CACHE_MAX_AGE and CACHE_MAX_STALE Headers time for api CACHE_MAX_AGE for online cache CACHE_MAX_STALE for offline cache

--- data/src/main/java/ir/example/newstest/data/restful/NewsJsonApi.kt
+++ data/src/main/java/ir/example/newstest/data/restful/NewsJsonApi.kt
@@ -2,10 +2,20 @@ package ir.example.newstest.data.restful
 
 import ir.example.newstest.domain.pojo.NewsEn
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
+
+const val CACHE_MAX_AGE = "CACHE_MAX_AGE"
+const val CACHE_MAX_STALE = "CACHE_MAX_STALE"
+private const val JSON_LIST_CACHE_MAX_AGE = 30  // 30 seconds
+private const val JSON_LIST_CACHE_MAX_STALE = 1 * 24 * 60 * 60   // 1 Days in seconds
 
 interface NewsJsonApi {
 
+    @Headers(
+        "$CACHE_MAX_AGE:$JSON_LIST_CACHE_MAX_AGE ",
+        "$CACHE_MAX_STALE:$JSON_LIST_CACHE_MAX_STALE"
+    )
     @GET("top-headlines")
     suspend fun getJsonNews(
         @Query("country") sources: String,
--- data/src/main/java/ir/example/newstest/data/restful/NewsXmlApi.kt
+++ data/src/main/java/ir/example/newstest/data/restful/NewsXmlApi.kt
@@ -2,9 +2,17 @@ package ir.example.newstest.data.restful
 
 import ir.example.newstest.domain.pojo.NewsFa
 import retrofit2.http.GET
+import retrofit2.http.Headers
+
+private const val XML_LIST_CACHE_MAX_AGE = 30  // 30 seconds
+private const val XML_LIST_CACHE_MAX_STALE = 1 * 24 * 60 * 60   // 1 Days in seconds
 
 interface NewsXmlApi {
 
+    @Headers(
+        "$CACHE_MAX_AGE:$XML_LIST_CACHE_MAX_AGE ",
+        "$CACHE_MAX_STALE:$XML_LIST_CACHE_MAX_STALE"
+    )
     @GET("rss/tp/10")
     suspend fun getXmlNews(): NewsFa
 
